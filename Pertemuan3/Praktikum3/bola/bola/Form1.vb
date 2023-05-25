31b743ca9203962052b0aea452541c4b012b84ee
Muhammad Rizky Sydik <103189894+sydik45@users.noreply.github.com>
2023-05-25 17:49:03 +0700

    Add files via upload

---
+++ Pertemuan3/Praktikum3/bola/bola/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Function HitungVolume(jari As Integer) As Integer
+        Dim volume As Integer
+        volume = 4 * 22 / 7 * jari * jari
+        Return volume
+    End Function
+
+    Private Function HitungLuasPermukaan(jari As Integer) As Integer
+        Dim luas_permukaan As Integer
+        luas_permukaan = (4 / 3) * 22 / 7 * jari * jari * jari
+        Return luas_permukaan
+    End Function
+
+    Private Sub btnhitung_Click(sender As Object, e As EventArgs) Handles btnhitung.Click
+        Dim jari, volume, luaspermukaan
+
+        jari = Val(txtjari.Text)
+
+        volume = HitungVolume(jari)
+        luaspermukaan = HitungLuasPermukaan(jari)
+
+        txtvolume.Text = Str(volume)
+        txtluaspermukaan.Text = Str(luaspermukaan)
+
+    End Sub
+End Class
+
