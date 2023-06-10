7568ac049f619d571d44ceb9f8746f7deda202ce
Muhammad Rizky Sydik <103189894+sydik45@users.noreply.github.com>
2023-06-10 16:58:11 +0700

    Add files via upload

---
+++ Pertemuan9/Latihan/AppAkademik/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.txtNIM = New System.Windows.Forms.TextBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtJK = New System.Windows.Forms.TextBox()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(48, 226)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(420, 204)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Teal
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(95, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(319, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Muhammad Rizky Sydik_200511013_TI20C"
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(279, 119)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 2
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'txtNIM
+        '
+        Me.txtNIM.Location = New System.Drawing.Point(127, 59)
+        Me.txtNIM.Name = "txtNIM"
+        Me.txtNIM.Size = New System.Drawing.Size(100, 20)
+        Me.txtNIM.TabIndex = 3
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(127, 101)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(100, 20)
+        Me.txtNama.TabIndex = 4
+        '
+        'txtJK
+        '
+        Me.txtJK.Location = New System.Drawing.Point(127, 145)
+        Me.txtJK.Name = "txtJK"
+        Me.txtJK.Size = New System.Drawing.Size(100, 20)
+        Me.txtJK.TabIndex = 5
+        '
+        'txtProdi
+        '
+        Me.txtProdi.Location = New System.Drawing.Point(127, 188)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(100, 20)
+        Me.txtProdi.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "NIM"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Nama"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 152)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Jenis Kelamin"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 191)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Prodi"
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.Location = New System.Drawing.Point(279, 52)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(75, 23)
+        Me.btnSimpan.TabIndex = 11
+        Me.btnSimpan.Text = "Simpan"
+        Me.btnSimpan.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(279, 186)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(75, 23)
+        Me.btnHapus.TabIndex = 12
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(515, 430)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnSimpan)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtProdi)
+        Me.Controls.Add(Me.txtJK)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.txtNIM)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtNIM As TextBox
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtJK As TextBox
+    Friend WithEvents txtProdi As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnSimpan As Button
+    Friend WithEvents btnHapus As Button
+End Class
---
+++ Pertemuan9/Latihan/AppAkademik/Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Reload()
+    End Sub
+
+    Private Sub Reload()
+        oMahasiswa.getAllData(DataGridView1)
+    End Sub
+
+    Private Sub txtNIM_Keydown(sender As Object, e As KeyEventArgs) Handles txtNIM.KeyDown
+        If (e.KeyCode = Keys.Enter) Then
+            oMahasiswa.Carimahasiswa(txtNIM.Text)
+            If (mahasiswa_baru = False) Then
+                TampilMahasiswa()
+            Else
+                MessageBox.Show("Data Tidak Ditemukan")
+                mahasiswa_baru = True
+            End If
+        End If
+
+    End Sub
+
+    Private Sub TampilMahasiswa()
+        txtNIM.Text = oMahasiswa.nim
+        txtNama.Text = oMahasiswa.nama
+        txtJK.Text = oMahasiswa.jk
+        txtProdi.Text = oMahasiswa.prodi
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ClearEntry()
+    End Sub
+
+    Private Sub ClearEntry()
+        txtNIM.Clear()
+        txtNama.Clear()
+        txtJK.Clear()
+        txtProdi.Clear()
+        txtNIM.Focus()
+    End Sub
+
+    Private Sub SimpanDatmahasiswa()
+        oMahasiswa.nim = txtNIM.Text
+        oMahasiswa.nama = txtNama.Text
+        oMahasiswa.jk = txtJK.Text
+        oMahasiswa.prodi = txtProdi.Text
+        oMahasiswa.Simpan()
+        Reload()
+        If (oMahasiswa.InsertState = True) Then
+            MessageBox.Show("Data berhasil Disimpan.")
+        ElseIf (oMahasiswa.UpdateState = True) Then
+            MessageBox.Show("Data berhasil Diperbarui.")
+        Else
+            MessageBox.Show("Data gagal disimpan")
+        End If
+        ClearEntry()
+
+    End Sub
+    Private Sub btnSimpan_Click(sender As Object, e As EventArgs) Handles btnSimpan.Click
+        If (txtNIM.Text <> "" And txtNama.Text <> "") Then
+            SimpanDatmahasiswa()
+            ClearEntry()
+            Reload()
+        Else
+            MessageBox.Show("nim dan nama tidak boleh kosong!")
+        End If
+
+    End Sub
+
+    Private Sub Hapus()
+        If (mahasiswa_baru = False And txtNIM.Text <> "") Then
+            oMahasiswa.Hapus(txtNIM.Text)
+            ClearEntry()
+            Reload()
+        End If
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        Dim jawab As Integer
+        jawab = MessageBox.Show("Apakah Data akan dihapus", "Confirm", MessageBoxButtons.YesNo)
+        If (jawab = vbYes) Then
+            Hapus()
+        Else
+            MessageBox.Show("Data batal dihapus")
+        End If
+    End Sub
+End Class
